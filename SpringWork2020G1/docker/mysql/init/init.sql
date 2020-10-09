b154ecd4d76de8f89b5382ee76987cdce592b190
{hanehaneland} <{a181408w@st.u-gakugei.ac.jp}>
2020-10-09 18:50:52 +0900

    update: init.sql Dockerfile

--- SpringWork2020G1/docker/mysql/init/init.sql
+++ SpringWork2020G1/docker/mysql/init/init.sql
@@ -50,39 +50,53 @@ CREATE TABLE `participates` (
   COLLATE = utf8_unicode_ci;
 
 CREATE TABLE `handles` (
-  `task_id` VARCHAR(10) NOT NULL,
+  `task_id` INT(10) NOT NULL,
   `prj_id` INT(10) NOT NULL,
   `user_id` VARCHAR(16) NOT NULL
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8
   COLLATE = utf8_unicode_ci;
 
-LOCK TABLES `users` WRITE;
-
-INSERT INTO `users`
-
-VALUES (`a181408w`, `近藤羽音`, `hanehane`), (`e185401f`, `市川晴也`, `seiyaseiya`), (`e185408w`, `佐藤央`, `oooooooo`), (`e185417m`, `渡邊幹斗`, `kantokanto`);
-
-UNLOCK　TABLES;
+-- LOCK TABLES `users` WRITE;
+INSERT INTO users
+VALUES ('a181408w', '近藤羽音', 'hanehane'),
+('e185401f', '市川晴也', 'seiyaseiya'),
+('e185408w', '佐藤央', 'oooooooo'),
+('e185417m', '渡邊幹斗', 'kantokanto');
+-- UNLOCK　TABLES;
 
 -- LOCK TABLES `projects` WRITE;
--- INSERT INTO `projects` (`title`, `overview`,　`host_id`, `deadline`,　`status`, `is_delayed`) VALUES (`親睦会`, `これからともにアプリ開発していくので懇親会をしましょう！`, `a181408w`, '2020-04-01 00:00:00', `2`, `true`), (`2020年G1春課題`, `研究室で使う共有ToDoリストのアプリ開発`, `e185401f`, '2020-10-16 00:00:00', `1`, `false`)；
+INSERT INTO projects (title, overview, host_id, deadline, status, is_delayed)
+VALUES ('親睦会', 'これからともにアプリ開発していくので懇親会をしましょう！', 'a181408w', '2020-04-01 00:00:00', 2, true),
+('2020年G1春課題', '研究室で使う共有ToDoリストのアプリ開発', 'e185401f', '2020-10-16 00:00:00', 1, false);
 -- UNLOCK　TABLES;
 
 -- LOCK TABLES `tasks` WRITE;
--- INSERT INTO `tasks` (`prj_id`, `title`,　`overview`, `deadline`,　`status`, `is_delayed`)
--- VALUES (`2`, `要求仕様書(MarkDown記法)`, `インスペクション回数は５回`, '2020-06-07 10:30:00', `2`, `false`),
--- (`2`, `画面シナリオ`, `インスペクション回数は3回`, '2020-06-22 10:00:00', `2`, `false`),
--- (`2`, `クラス図`, `インスペクション回数は3回`, '2020-07-06 09:25:00', `2`, `false`),
--- (`2`, `DB設計書`, `インスペクション回数は3回`, '2020-07-13 12:00:00', `2`, `false`),
--- (`2`, `シーケンス図`, `インスペクション回数は1人3回`, '2020-07-20 14:30:00', `1`, `true`),
--- (`2`, `実装`, `未定`, '2020-10-16 00:00:00', `1`, `false`);
+INSERT INTO tasks (prj_id, title,　overview, deadline, status, is_delayed)
+VALUES (2, '要求仕様書(MarkDown記法)', 'インスペクション回数は５回', '2020-06-07 10:30:00', 2, false),
+(2,'画面シナリオ', 'インスペクション回数は3回', '2020-06-22 10:00:00', 2, false),
+(2, 'クラス図', 'インスペクション回数は3回', '2020-07-06 09:25:00', 2, false),
+(2, 'DB設計書', 'インスペクション回数は3回', '2020-07-13 12:00:00', 2, false),
+(2, 'シーケンス図', 'インスペクション回数は1人3回', '2020-07-20 14:30:00', 1, true),
+(2, '実装', '未定', '2020-10-16 00:00:00', 1, false);
 -- UNLOCK　TABLES;
 
 -- LOCK TABLES `participates` WRITE;
--- INSERT INTO `participates` VALUES (`a181408w`, `1`, `true`), (`a181408w`, `2`, `false`), (`e185401f`, `1`, `false`), (`e185401f`, `2`, `true`), (`e185408w`, `1`, `false`), (`e185408w`, `2`, `false`), (`e185417m`, `1`, `false`), (`e185417m`, `2`, `false`)；
--- UNLOCK　TABLES;
+INSERT INTO participates
+VALUES ('a181408w', 1, true),
+('a181408w', 2, false),
+('e185401f', 1, false),
+('e185401f', 2, true),
+('e185408w', 1, false),
+('e185408w', 2, false),
+('e185417m', 1, false),
+('e185417m', 2, false);
+-- UNLOCK TABLES;
 
 -- LOCK TABLES `handles` WRITE;
--- INSERT INTO `handles` VALUES (`1`, `2`, `e185417m`), (`2`, `2`, `a181408w`), (`3`, `2`, `e185408w`), (`4`, `2`, `e185401f`)；
+INSERT INTO handles
+VALUES (1, 2, 'e185417m'),
+(2, 2, 'a181408w'),
+(3, 2, 'e185408w'),
+(4, 2, 'e185401f');
 -- UNLOCK　TABLES;
